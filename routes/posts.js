2ce606ca4c3cd4edfbe13589559b69a16312d4b6
Yoseb Han <yosebseb91@gmail.com>
2022-12-01 12:27:11 +0900

    Update posts.js
    날짜 내림차순 추가

--- routes/posts.js
+++ routes/posts.js
@@ -19,7 +19,7 @@ router.post("/", async (req, res) => {
 // 게시글 조회
 router.get("/", async (req, res) => {
     try {
-        const posting = await Posts.find();
+        const posting = await Posts.find().sort({ createdAt: -1 });
         const results = posting.map((posts) => {
             return {
                 postId: posts._id,
